7c3c709a29d86095c11b6698eca319ae45f18365
Anishaprabhu019 <145418718+Anishaprabhu019@users.noreply.github.com>
2023-12-29 19:05:28 +0530

    Update Login.java

--- src/test/java/com/company/Login.java
+++ src/test/java/com/company/Login.java
@@ -48,7 +48,7 @@ public class Login {
 	    username.sendKeys("rajattiwari92@gmail.com");
 	    password.sendKeys("tiwari@2");
 	    Login.click();
-	    Thread.sleep(3000);
+	    
 	    //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	    WebElement navigationclick = driver.findElement(By.id("logoutMenu"));
 	    WebElement logout = driver.findElement(By.xpath("//div[@id='u_d_1']/div/div/div/div/div/ul/li[12]/a/span/span"));
